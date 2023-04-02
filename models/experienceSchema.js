d67f81effd65defa78f810cb5947d5a0122e32d8
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-02 09:34:24 +0600

    experience Schema

---
+++ models/experienceSchema.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const experienceSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    period: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const Experience = mongoose.model("experience", experienceSchema);
+module.exports = Experience;
