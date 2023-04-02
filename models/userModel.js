3a80945c489730fb339f9ffbc1630b328391f731
Ismile Sardar <98180464+ismailsardar@users.noreply.github.com>
2023-04-02 09:14:45 +0600

    user model create

---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("users", userSchema);
+module.exports = User;
